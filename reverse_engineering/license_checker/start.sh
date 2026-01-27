2f2265293f736c511db10648c66c7924f7a05f92
Pranavbarathi05 <pranavbarathi05@gmail.com>
2026-01-27 23:03:52 +0530

    corrected to socat

--- reverse_engineering/license_checker/start.sh
+++ reverse_engineering/license_checker/start.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-
-# Start the license checker as a network service
-while true; do
-    ./license_checker | nc -l 8002
-done
+socat TCP-LISTEN:8002,reuseaddr,fork EXEC:"timeout 30 ./license_checker",pty,stderr,setsid,sigint,sane
