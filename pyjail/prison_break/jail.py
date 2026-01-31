15c5f0cfd7821aad19530bdc8c188424fd67ef99
Pranavbarathi05 <pranavbarathi05@gmail.com>
2026-01-31 13:48:06 +0530

    corrected prison break

--- pyjail/prison_break/jail.py
+++ pyjail/prison_break/jail.py
@@ -1,52 +1,41 @@
-#!/usr/bin/python
-
+#!/usr/bin/env python3
 import sys
 
-class Sandbox(object):
-    def execute(self, code_string):
-        exec(code_string)
+FLAG = "dscctf{m1c34l_sc0fi3ld_fr0m_pr1s0n_br34k}"
+
+SAFE_BUILTINS = {
+    # constants
+    "True": True,
+    "False": False,
+    "None": None,
+
+    # functions allowed
+    "abs": abs,
+    "bin": bin,
+    "dir": dir,
+    "help": help,
+    "str": str,
+    "int": int,
+    "len": len,
+    "print": print,
+}
+
+class Sandbox:
+    def execute(self, code):
+        exec(code, {"__builtins__": SAFE_BUILTINS, "FLAG": FLAG})
         sys.stdout.flush()
 
 sandbox = Sandbox()
 
-_raw_input = raw_input
-
-main = sys.modules["__main__"].__dict__
-orig_builtins = main["__builtins__"].__dict__
-
-builtins_whitelist = set((
-    #exceptions
-    'ArithmeticError', 'AssertionError', 'AttributeError', 'Exception',
-
-    #constants
-    'False', 'None', 'True',
-
-    #types
-    'basestring', 'bytearray', 'bytes', 'complex', 'dict',
-
-    #functions
-    'abs', 'bin', 'dir', 'help'
-
-    # blocked: eval, execfile, exit, file, quit, reload, import, etc.
-))
-
-for builtin in orig_builtins.keys():
-    if builtin not in builtins_whitelist:
-        del orig_builtins[builtin]
-
 print("Find the flag.")
 sys.stdout.flush()
 
-def flag_function():
-    flag = "dscctf{m1c34l_sc0fi3ld_fr0m_pr1s0n_br34k}"
-
-while 1:
+while True:
     try:
         sys.stdout.write(">>> ")
         sys.stdout.flush()
-        code = _raw_input()
+        code = input()
         sandbox.execute(code)
-
     except Exception:
         print("You have encountered an error.")
         sys.stdout.flush()
